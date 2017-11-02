4a37034781c27cf7f9c917d3cbc48899a9ccf51b
Smerth <merth@me.com>
2017-11-02 12:25:15 -0700

    finished mockup

---
+++ readme.md
@@ -0,0 +1,3 @@
+# Mockup of Coconut News
+
+Quick mock up of an article page and a contributor page for a Drupal 8 theme using Foundation.
---
+++ src/assets/js/app.js
@@ -0,0 +1,31 @@
+import $ from 'jquery';
+import whatInput from 'what-input';
+
+window.$ = $;
+
+import Foundation from 'foundation-sites';
+// If you want to pick and choose which modules to include, comment out the above and uncomment
+// the line below
+//import './lib/foundation-explicit-pieces';
+
+
+$(document).foundation(
+
+
+
+
+
+ );
+
+// $(function() {
+//   $(window).scroll(function() {
+//     var winTop = $(window).scrollTop();
+//     if (winTop >= 30) {
+//       $("body").addClass("sticky-shrinknav-wrapper");
+//     } else{
+//       $("body").removeClass("sticky-shrinknav-wrapper");
+//     }
+//   });
+// });
+
+
